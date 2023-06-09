c0f2a0a990548dbec3a7a4a16e2980cdf6a60760
Deepak <ideepakrabindra@gmail.com>
2023-06-09 11:21:09 +0530

    Backend Connection Done

---
+++ Backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./db/db");
+
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const server = () => {
+  db();
+  app.listen(PORT, () => {
+    console.log("Server Connected to port: ", PORT);
+  });
+};
+
+server();
