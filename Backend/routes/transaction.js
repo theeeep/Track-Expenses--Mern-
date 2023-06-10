94350f184c69fd7ff891c934d12b95e2335664d3
Deepak <ideepakrabindra@gmail.com>
2023-06-10 20:08:05 +0530

    remove gitignore files

--- Backend/routes/transaction.js
+++ Backend/routes/transaction.js
@@ -1,13 +1,23 @@
-const { addExpense, getExpense, deleteExpense } = require("../controllers/expenseController");
-const { addIncome, getIncomes, deleteIncome } = require("../controllers/incomeController");
+const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+} = require("../controllers/expenseController");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/incomeController");
 
 const router = require("express").Router();
 
-router.post("/add-income", addIncome)
-    .get('/get-income',getIncomes)
-    .delete('/delete-income/:id',deleteIncome)
-    .post('/add-expense',addExpense)
-    .get('/get-expense',getExpense)
-    .delete('/delete-expense/:id',deleteExpense)
+router;
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpense)
+  .delete("/delete-expense/:id", deleteExpense);
 
 module.exports = router;
